54dbf2a8af573fd6b9b784336b5d06732d810799
frolovsky <apf75@yandex.ru>
2020-10-05 12:29:37 +0300

    Add path package

--- task1/utils/args-validator.js
+++ task1/utils/args-validator.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 class ArgumentsValidator {
   constructor({ input = '', output = '', shift = '', action = ''} = {}) {
     Object.assign(this, {input, output, shift, action});
+    if (this.input !== '') {
+      this.input = path.resolve(this.input)
+    }
+    if (this.output !== '') {
+      this.output = path.resolve(this.output)
+    }
   }
   shiftValidate() {
     return !!this.shift.match(/^(-)?\d+$/)
